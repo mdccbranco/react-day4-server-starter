918c093f2176e6fe25f4204921781528c6ba609b
monicadamasceno <monicadamasceno@gmail.com>
2023-10-24 19:57:35 -0300

    update cors

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.locals.title = 'Pesquisa';
 app.use(
   cors({
     credentials: true,
-    origin: ['http://localhost:3000'] // <== this will be the URL of our React app (it will be running on port 3000)
+    origin: ['https://ifb-pesquisa-industrias-df.vercel.app'] // <== this will be the URL of our React app (it will be running on port 3000)
   })
 );
 
